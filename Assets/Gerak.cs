2195a17a11d38ee1c1edea28889cf535f95d2401
DaffaIrawan <irawandaffa910@gmail.com>
2024-08-28 14:35:13 +0700

    gerak

--- Assets/Gerak.cs
+++ Assets/Gerak.cs
@@ -2,17 +2,29 @@ using UnityEngine;
 
 public class Gerak : MonoBehaviour
 {
+    Rigidbody2D rb;
     public float movementSpeed = 5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (rb == null)
+        {
+            Debug.LogError("No Rigidbody2D component found on this GameObject.");
+        }
+    }
 
     void Update()
     {
-        // Read horizontal input
+        // Get input from both horizontal (left/right) and vertical (up/down) axes
         float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Apply movement based on input
+        rb.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
 
         // Debugging
-        Debug.Log("Horizontal Input: " + horizontal);
-
-        // Move the GameObject based on input
-        transform.Translate(new Vector2(horizontal * movementSpeed * Time.deltaTime, 0));
+        Debug.Log("Current Velocity: " + rb.velocity);
     }
 }
